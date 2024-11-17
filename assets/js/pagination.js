24b5067de2c90d3106a586bf5e74b26e6a3acf41
Murka <muratahmethanov@gmail.com>
2024-11-17 16:25:14 +0300

    refactor: implement separate click handler

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,44 +1,52 @@
 const blocksPerPage = 3;
 
+const handlePageButtonClick = (event) => {
+  const loader = getLoadingIndicator();
+  const attractionCardsContainer = document.querySelector(".main__blocks");
+  const buttonNumber = event.target.getAttribute('data-page');
+  const urlObject = getUrlObject(cityGuideApiUrl, buttonNumber, blocksPerPage);
+
+  attractionCardsContainer.innerHTML = "";
+  attractionCardsContainer.insertAdjacentElement('beforebegin', loader);
+
+  fetchAttractionsData(urlObject)
+    .then((data) => {
+      if (data) {
+        showAttractionCards(data);
+      }
+    })
+    .catch((error) => {
+      const errorMessageElement = document.createElement("span");
+
+      errorMessageElement.textContent = error;
+      attractionCardsContainer.append(errorMessageElement);
+    })
+    .finally(() => {
+      loader.remove();
+    });
+};
+
+const debouncedHandlePageButtonClick = debounce(handlePageButtonClick);
+
 const showPaginationButtons = (totalItemsCount, blocksPerPage) => {
   const buttonsCount = Math.ceil(totalItemsCount / blocksPerPage);
   const paginationButtonsContainer = document.querySelector(".main__pagination");
 
-  paginationButtonsContainer.innerHTML = '';
+  if (paginationButtonsContainer) {
+    paginationButtonsContainer.innerHTML = '';
 
-  for (let i = 0; i < buttonsCount; i += 1) {
-    const button = document.createElement('button');
-    const buttonNumber = i + 1;
+    for (let i = 0; i < buttonsCount; i += 1) {
+      const button = document.createElement('button');
+      const buttonNumber = i + 1;
 
-    button.className = "main__pagination-btn";
-    button.dataset.page = buttonNumber;
-    button.textContent = buttonNumber;
+      button.className = "main__pagination-btn";
+      button.dataset.page = buttonNumber;
+      button.textContent = buttonNumber;
+      button.addEventListener('click', (event) => {
+        debouncedHandlePageButtonClick(event);
+      });
 
-    button.addEventListener('click', () => {
-      const loader = getLoadingIndicator();
-      const attractionCardsContainer = document.querySelector(".main__blocks");
-      const urlObject = getUrlObject(cityGuideApiUrl, buttonNumber, blocksPerPage);
-
-      attractionCardsContainer.innerHTML = "";
-      attractionCardsContainer.insertAdjacentElement('beforebegin', loader);
-
-      fetchAttractionsData(urlObject)
-        .then((data) => {
-          if (data) {
-            showAttractionCards(data);
-          }
-        })
-        .catch((error) => {
-          const errorMessageElement = document.createElement("span");
-
-          errorMessageElement.textContent = error;
-          attractionCardsContainer.append(errorMessageElement);
-        })
-        .finally(() => {
-          loader.remove();
-        });
-    });
-
-    if (paginationButtonsContainer) paginationButtonsContainer.append(button);
+      paginationButtonsContainer.append(button);
+    }
   }
 };
