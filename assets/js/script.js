cac66a58aa5446e635c3124bee295df78e809a5b
Murka <muratahmethanov@gmail.com>
2024-12-15 23:38:29 +0300

    fix: slider using OOP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,34 @@
-let slideIndex = 1;
-
-const showSlides = (n) => {
-  let slides = document.getElementsByClassName("item");
-
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length 
+class Slider {
+  constructor(slideClass) {
+    this.slides = document.getElementsByClassName(slideClass);
+    this.slideIndex = 1;
+    this.showSlides(this.slideIndex);
   }
 
-  for (let slide of slides) {
-    slide.style.display = "none";
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+
+    for (let slide of this.slides) {
+      slide.style.display = "none";
+    }
+    this.slides[this.slideIndex - 1].style.display = "block";
   }
-  slides[slideIndex - 1].style.display = "block";
-};
 
-const nextSlide = () => {
-  showSlides(slideIndex += 1);
-};
+  nextSlide() {
+    this.showSlides(this.slideIndex += 1);
+  }
 
-const previousSlide = () => {
-  showSlides(slideIndex -= 1);
-};
+  previousSlide() {
+    this.showSlides(this.slideIndex -= 1);
+  }
+}
 
-showSlides(slideIndex);
+const slider = new Slider("item");
 
-
-
-
+document.getElementById("next").addEventListener("click", () => slider.nextSlide());
+document.getElementById("prev").addEventListener("click", () => slider.previousSlide());
