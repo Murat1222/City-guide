a055a75436ffb97bb43e1dfa5c867156a9422c23
Murka <muratahmethanov@gmail.com>
2024-12-17 17:54:15 +0300

    fix: getUrlObject changed

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -24,13 +24,15 @@ function debounce(fn, timeoutMs = 250) {
   }
 };
 
-function getUrlObject(url, page, limit, title, category) {
+function getUrlObject(url, page, limit, title, category, sortBy, order) {
   const urlObject = new URL(url);
 
   if (page) urlObject.searchParams.append('page', page);
   if (limit) urlObject.searchParams.append('limit', limit);
   if (title) urlObject.searchParams.append('title', title);
   if (category) urlObject.searchParams.append('category', category);
+  if (sortBy) urlObject.searchParams.append('sortBy', sortBy);
+  if (order) urlObject.searchParams.append('order', order);
 
   return urlObject;
-};
+}
